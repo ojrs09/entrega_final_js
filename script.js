c5ae07ad1b3ad6dea0460041b85d5b2b15641797
ojrs09 <124927398+ojrs09@users.noreply.github.com>
2023-04-14 20:26:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,116 +1,144 @@
 // Elementos DOM necesarios
 
-const 
-    mailLogin = document.getElementById('emailLogin'),
-    passLogin = document.getElementById('passwordLogin'),
+const
     btnLogin = document.getElementById('login'),
-    btnAgregar=document.getElementById('agregarNota')
+    btnAgregar = document.getElementById('agregarNota')
     contTarjetas = document.getElementById('tarjetas'),
-    toggles = document.querySelectorAll('.toggles')
+    toggles = document.querySelectorAll('.toggles'),
+    form = document.querySelector(".login")
 
-// Validación de usuario (Usando archivo .json)
-    let array=[]
-    let encontrado = fetch('maestros.json')
-    .then(response => response.json())
-    .then((data)=>{
-        array=data
-        validarUsuario(array)
+    // Librería
+    Swal.fire('Bienvenido a tu programa de estudiantes')
+
+// Array de estudiantes (se hizo así ya que los maestros los hice con fetch, así tengo ambas vias)
+
+    const estudiantes = [{
+        nombre: 'Omar',
+        apellido: 'Romero',
+        edad: 15,
+        estatura: 1.62,
+        img: './multimedia/web_of_Spider-Man_Vol_1_129.1_Sin_texto.webp'
+        },
+        {
+        nombre: 'Karla',
+        apellido: 'Casanas',
+        edad: 14,
+        estatura: 1.25,
+        img: './multimedia/sailor-moon.jpg'
+        },
+        {
+        nombre: 'Edward',
+        apellido: 'Bastidas',
+        edad: 12,
+        estatura:1.45,
+        img: './multimedia/guUYd44L_400x400.png'
+        },
+        {
+        nombre: 'Yeespri',
+        apellido: 'Morales',
+        edad: 17,
+        estatura: 1.65,
+        img: './multimedia/descarga.jpg'
+        },
+        {nombre: 'Diosa',
+        apellido: 'Yglesias',
+        edad: 15, 
+        estatura: 1.49,
+        img: './multimedia/Jean_Francois_Armand_Felix_Bernard_-_Fortuna.jpg'
+        },
+        {
+        nombre: 'Juan',
+        apellido: 'Casanas',
+        edad: 12,
+        estatura: 1.27,
+        img: './multimedia/Unnamed_29.webp'
+    }]
+
+// Agrego elementos necesarios al formulario y validación de usuarios
+
+form.addEventListener("submit", (e) => {
+
+    e.preventDefault()
+
+    const mailLogin = document.getElementById('emailLogin').value
+    const passLogin = document.getElementById('passwordLogin').value
+
+    fetch('./maestros.json')
+
+        .then(response => response.json())
+        .then((data) => {
+            validarUsuario(data, mailLogin, passLogin)
         })
-        
-        function validarUsuario(array,mail,pass){
-        console.log(array)
-            let usuario = array.find(array => array.mail == mail);
-            if (typeof usuario === 'undefined') {
-                return false;
-            } else {
-                if (array.pass != pass) {
-                    
-                    return false;
-                } else {
-                    return usuario;
-                    
-                }
-            }
+})
+
+function validarUsuario(array, mail, pass) {
+
+    console.log(array)
+    let usuario = array.find(maestro => maestro.mail == mail);
+
+    if (typeof usuario === 'undefined') {
+        return false;
+        // alert('Ingreso incorrecto')
+    } else {
+        if (usuario.pass != pass) {
+            // alert ('ingreso incorrecto')
+            return false;
+        } else {
+            return usuario;
+            // alert ('ingreso correcto')
+
         }
-
-//Guardamos los datos que recuperamos de la database en el local storage
-
-// btnLogin.addEventListener('click',()=>{
-//     localStorage.setItem('email',emailLogin.value),
-//     localStorage.setItem('pass', passwordLogin.value)
-//     })
-
-
-
-//Recupero los datos que se guardaron y los retorno
-
-// function recuperarUsuario(localstorage) {
-// let usuarioEnStorage = JSON.parse(localstorage.getItem('email'));
-// return usuarioEnStorage;
-// }
-
-// function recuperarClave(localstorage) {
-// let claveEnStorage = JSON.parse(localstorage.getItem('pass'));
-// return claveEnStorage;
-// }
-
+    }
+}
 
 // --------------------------------CREA HTML--------------------
 
-// function mostrarInfoEstudiantes(array) {
-// contTarjetas.innerHTML = '';
-// array.forEach(element => {
-//     let html = `<div class="card cardEstudiante" id="tarjeta${element.nombre}">
-//             <h3 class="card-header" id="nombreEstudiante">Nombre: ${element.nombre}</h3>
-//             <img src="${element.img}" alt="${element.nombre}" class="card-img-bottom" id="fotoEstudiante">
-//             <div class="card-body">
-//                 <p class="card-text" id="apellidoEstudiante">Apellido: ${element.apellido} </p>
-//                 <p class="card-text" id="edadEstudiante">Edad: ${element.edad} años</p>
-//                 <button type="submit" class="btn btn-primary" id="agregarNota" >agregarNota</button> 
-//             </div>
-//         </div>`;
-//     contTarjetas.innerHTML += html;
+function mostrarInfoEstudiantes(array) {
+    contTarjetas.innerHTML = '';
+    array.forEach(element => {
+        let html = `<div class="card cardEstudiante" id="tarjeta${element.nombre}">
+                <h3 class="card-header" id="nombreEstudiante">Nombre: ${element.nombre}</h3>
+                <img src="${element.img}" alt="${element.nombre}" class="card-img-bottom" id="fotoEstudiante">
+                <div class="card-body">
+                    <p class="card-text" id="apellidoEstudiante">Apellido: ${element.apellido} </p>
+                    <p class="card-text" id="edadEstudiante">Edad: ${element.edad} años</p>
+                    <button type="submit" class="btn btn-primary" id="agregarNota" >agregarNota</button> 
+                </div>
+            </div>`;
+        contTarjetas.innerHTML += html;
 
-// });
-// }
+    });
+}
 
 // ---------------------------VISUALIZACIÓN-----------------------------
 
-// function presentarInfo(array, clase) {
-//     array.forEach(element => {
-//         element.classList.toggle(clase);
-//     });
-// }
+function presentarInfo(array, clase) {
+    array.forEach(element => {
+        element.classList.toggle(clase);
+    });
+}
 
-// --------------------------------------------------------------------
+// ---------------------REVISAR ESTA PARTE-----------------------------------------------
+// Así lo tenía en mi proyecto anterior pero ahora lo traigo a este y no me funciona
 
+if (!mailLogin.value || !passLogin.value) {
+    alert('Todos los campos son requeridos');
+} else {
+    
+    let array = validarUsuario(maestros, mailLogin.value, passLogin.value);
 
-// Llamamamos al botón de login con el objeto del evento y usar preventDefault
-
-// btnLogin.addEventListener('click', (evt) => {
-//     evt.preventDefault();
-
-    //Validamos que ambos campos estén completos.
-
-//     if (!mailLogin.value || !passLogin.value) {
-//         alert('Todos los campos son requeridos');
-//     } else {
-        
-//         let data = validarUsuario(array, mailLogin.value, passLogin.value);
-
-//         if (!data) {
-//             alert(`Usuario o contraseña erróneos`);
-//         } else {
-//             //Muestro la info para usuarios logueados (con toggles)
-//             mostrarInfoEstudiantes(estudiantes);
-//             presentarInfo(toggles, 'd-none');
-//         }
-//     }
-// });
+    if (!array) {
+        alert(`Usuario o contraseña erróneos`);
+    } else {
+        //Muestro la info para usuarios logueados (con toggles)
+        mostrarInfoEstudiantes(estudiantes);
+        presentarInfo(toggles, 'd-none');
+    }
+};
 
 //Limpiar los storages
 
-// function borrarDatos() {
-//     localStorage.clear();
-//     sessionStorage.clear();
-// }
+function borrarDatos() {
+    localStorage.clear();
+    sessionStorage.clear();
+}
